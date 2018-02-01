0f738e19244d74b2db899b3b95cfcbd1a4b92af2
Noga <>
2018-02-01 16:06:20 +0200

    Add fields in Purchase.java

--- src/main/java/JDBC_DAO/entity/Purchase.java
+++ src/main/java/JDBC_DAO/entity/Purchase.java
@@ -7,5 +7,66 @@ package JDBC_DAO.entity;
  */
 public class Purchase {
     private int purchaseId;
+    private int count;
+    private int customerId;
+    private int ProductId;
+
+    /**
+     * @return the purchaseId
+     */
+    public int getPurchaseId() {
+        return purchaseId;
+    }
+
+    /**
+     * @param purchaseId the purchaseId to set
+     */
+    public void setPurchaseId(int purchaseId) {
+        this.purchaseId = purchaseId;
+    }
+
+    /**
+     * @return the count
+     */
+    public int getCount() {
+        return count;
+    }
+
+    /**
+     * @param count the count to set
+     */
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    /**
+     * @return the customerId
+     */
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    /**
+     * @param customerId the customerId to set
+     */
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    /**
+     * @return the ProductId
+     */
+    public int getProductId() {
+        return ProductId;
+    }
+
+    /**
+     * @param ProductId the ProductId to set
+     */
+    public void setProductId(int ProductId) {
+        this.ProductId = ProductId;
+    }
+    
+    
     
 }
